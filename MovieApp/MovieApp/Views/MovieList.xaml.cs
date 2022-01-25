c2882589ef13a44b4b77090b877068dd840776f7
nomore127 <mczyz@edu.cdv.pl>
2022-01-25 10:05:30 +0100

    Add MovieList
    Added MovieList

---
+++ MovieApp/MovieApp/Views/MovieList.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MovieApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MovieList : ContentPage
+    {
+        public MovieList()
+        {
+            InitializeComponent();
+          
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+            using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+            {
+                conn.CreateTable<Movie>();
+                var posted = conn.Table<Movie>().ToList();
+                MovieListView.ItemsSource = posted;
+            }
+        
+        }
+
+    }
+}
