717e2a83adf7dca02017ca673877911505fae693
nomore127 <mczyz@edu.cdv.pl>
2022-01-25 21:32:49 +0100

    MovieList Updated

---
+++ MovieApp/MovieApp/Models/MovieList.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace MovieApp.Models
+{
+  public class MovieListModel
+	{
+		public string Title { get; set; }
+		public string Overview { get; set; }
+		public double Vote_average { get; set; }
+		public string Category { get; set; }
+		public ImageSource Poster_path { get; set; }
+		public string Backdrop_path { get; set; }
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+	}
+}
---
+++ MovieApp/MovieApp/Models/WatchLaterMovies.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MovieApp.Models
+{
+   public class WatchLaterMovies
+    {
+		public string Title { get; set; }
+		public string Overview { get; set; }
+		public double Vote_average { get; set; }
+		public string Category { get; set; }
+		public string Poster_path { get; set; }
+		public string Backdrop_path { get; set; }
+		[PrimaryKey, AutoIncrement]
+		public int Id { get; set; }
+
+	}
+}
